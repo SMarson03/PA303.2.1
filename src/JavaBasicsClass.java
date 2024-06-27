83a7912e5a36843f02658a7e1809b9fdc3c7f76b
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-06-27 10:56:37 -0400

    1

---
+++ src/JavaBasicsClass.java
@@ -0,0 +1,51 @@
+public class JavaBasicsClass {
+    public static void main(String[] args){
+        //Write a program that declares two integer variables, assigns an
+        // integer to each, and adds them together. Assign the sum to a
+        // variable. Print out the result.
+
+        int num1 = 22;
+        int num2 = 8;
+
+        int sum = num1 + num2;
+
+        System.out.println( "The sum of" + num1 + "and" + num2 + "is" + sum );
+
+        //	Write a program that declares two double variables, assigns a
+        //	number to each, and adds them together. Assign the sum to a
+        //	variable. Print out the result.
+
+        double numb1 = 2.50;
+        double numb2 = 1.50;
+        double dsum = numb1 + numb2;
+
+        System.out.println("The sum of" + numb1 + "and" + numb2 + "is" + dsum);
+
+//Write a program that declares an integer variable and a double variable,
+//assigns numbers to each,// and adds them together. Assign the sum to a variable.
+// Print out the result. What variable type must the sum be?
+
+    int value =8;
+    double dvalue = 2.5;
+    double dsum1 = value + dvalue;
+
+        System.out.println("Sum: " +dsum1);
+
+//Write a program that declares two integer variables, assigns an integer to each, and
+// divides the larger number by the smaller number. Assign the result to a variable.
+// Print out the result. Now change the larger number to a decimal. What happens? What
+// corrections are needed?
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
